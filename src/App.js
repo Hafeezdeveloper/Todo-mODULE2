d92f06653880168f682923d57b1d8b8eca4978c0
Hafeezdeveloper <Hafeez.hussain2003@gmail.com>
2023-01-14 12:39:57 +0500

    Todo app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import avvvv from './mode1.mjs'
+// import Navbar from './components/Navbar';
+import React,{ useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [txt,setTxt] = useState("")
+  const [array,setArrayList] = useState([])
+  
+  const addBtn= () =>{
+    setArrayList([
+      ...array, 
+      {value:txt}
+    ])
+    setTxt("")
+  }
+
+  const delBtn = (ind) =>{
+      const arraCpy = [...array]
+      const arrayFilt = arraCpy.filter( (data , i ) =>{ 
+        if(ind !== i){
+          return{
+            ...data
+          }
+        }
+      })
+        // console.log(data)
+      //   if(i !== ind){
+      //     return {
+      //       ...data,
+      //     }
+      //   }                                
+      // })
+      setArrayList(arrayFilt)
+  }
+
+  return (    
+    <>
+    
+    <div style={{display:"flex",justifyContent:"center",marginTop:"50px"}}>
+      <input style={{width:"50%",height:'50px',fontSize:'20px'}} type="text" value={txt} onChange={(e) => setTxt(e.target.value)} name="" id="" />
+      <button style={{backgroundColor:"red",color:"white"}} onClick={addBtn}>Add</button>    
     </div>
-  );
+    <div>
+      <ul style={{margin:'10px 320px'}}>
+        {array.map( (data , ind) =>{
+          // console.log(data)
+          return(
+            <li key={ind}> <h3>{data.value}</h3> <button >edit</button> <button onClick={() =>delBtn(ind)}>del</button></li>
+          )
+        })}
+      </ul>
+    </div>
+    </>
+      );
 }
 
 export default App;
---
+++ src/mode1.mjs
@@ -0,0 +1,9 @@
+//  let a =  "hafeez";
+//  let b =  "ali";
+//  let c =  "waqar";
+//  let d =  "hamza";
+
+//  export default a;
+//  export {b};
+//  export {c};
+//  export {d};
